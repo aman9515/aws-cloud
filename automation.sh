62283b81bc24358c7937627a87ad811b9f47d8b3
roshan <roshan5232@gmail.com>
2026-08-16 10:22:00 +0000

    updating automation.sh file to branch

---
+++ automation.sh
@@ -0,0 +1,20 @@
+rchitecture of Git
+* Installation of Git
+* Basic Git commands
+   * git version
+   * git init
+   * git add
+   * git commit
+   * git status
+   * git log
+   * git config
+   * git show
+   * git rm
+--------------------------------------------------------------------------------------------------------------
+* Branches
+* Tags
+--------------------------------------------------------------------------------------------------------------
+* Merge & Rebase
+* Cherry-pick
+--------------------------------------------------------------------------------------------------------------
+* Github Account creation
